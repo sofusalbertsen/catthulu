89c6a1387fdaf5120fa7600273a304fb9f793833
Sofus Albertsen <soa@praqma.com>
2021-01-19 14:33:20 +0100

    made commit script use variables

--- makecommit.sh
+++ makecommit.sh
@@ -1,4 +1,4 @@
-echo "more text" >> sofus.txt
-git add sofus.txt
-git commit -m "another commit"
+echo "more text" >> "$GIT_NAME.txt"
+git add $GIT_NAME.txt
+git commit -m "another commit by $GIT_NAME"
 git push
